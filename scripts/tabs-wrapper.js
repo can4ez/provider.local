17bb5824a36b157acc9768d072312bdc5766c1f3
Joflish <joflish@yandex.ru>
2024-11-15 13:10:20 +0500

    Стрелочки тоже исправил

--- scripts/tabs-wrapper.js
+++ scripts/tabs-wrapper.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Находим все вкладки и контейнеры с карточками
     const tabs = document.querySelectorAll(".tabs-wrapper");
     const containers = {
         "home-internet": document.getElementById("home-internet-slider"),
-        "home-tv": document.getElementById("home-tv-slider"), // Исправлено с "TV"
+        "home-tv": document.getElementById("home-tv-slider"),
         "home-telephone": document.getElementById("home-telephone-slider"),
-        "home-sput-tv": document.getElementById("home-sput-tv-slider"), // Исправлено с "TV-packages"
+        "home-sput-tv": document.getElementById("home-sput-tv-slider"),
     };
+    const btnContainer = document.querySelector('.swiper-container-btn');
 
     // Функция для показа карточек
     function showContainer(targetId) {
@@ -19,17 +19,21 @@ document.addEventListener("DOMContentLoaded", () => {
         if (containers[targetId]) {
             containers[targetId].style.display = "block";
         }
+
+        // Отображаем кнопки навигации только для "Домашний телефон"
+        if (targetId === "home-telephone") {
+            btnContainer.style.display = "flex";
+        } else {
+            btnContainer.style.display = "none";
+        }
     }
 
     // Назначаем обработчики кликов для вкладок
     tabs.forEach((tab) => {
         tab.addEventListener("click", () => {
-            // Убираем класс активной вкладки у всех
             tabs.forEach((t) => t.classList.remove("active-tab"));
-            // Добавляем класс активной вкладки к текущей
             tab.classList.add("active-tab");
 
-            // Получаем ID целевой вкладки из data-target и показываем соответствующий контейнер
             const targetId = tab.dataset.target;
             showContainer(targetId);
         });
